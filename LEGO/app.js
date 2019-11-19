ad19fa7073c3b5613715c3cbaebb5a1764b7c4e0
ArmijoAlessio <armijo.ramirez.alessio.michele@iisgalvanimi.edu.it>
2019-11-19 11:59:57 +0000

    commit

---
+++ LEGO/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const people = require('./lego.json'); //Copia il file people.json dentro la variabile people
+
+var app = express();
+
+app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
+
+app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
+
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
+app.get('/', function (req, res) {
+  res.render('index', {
+   title: 'scegli il tuo lego',
+    lego: lego.legos //Passa il vettore profiles alla pagina index.pug
+
+ });
+});
